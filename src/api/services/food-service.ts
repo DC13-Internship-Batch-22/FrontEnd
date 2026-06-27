b9f6efabfc87e58a3a90ccc888a15659c80ab788
ngoduong1740 <96032665+ngoduong1740@users.noreply.github.com>
2026-06-27 11:58:46 +0700

    fix: remove unused import

--- src/api/services/food-service.ts
+++ src/api/services/food-service.ts
@@ -5,4 +5,15 @@ export const foodService = {
     const response = await apiClient.get('/foods');
     return response.data;
   },
+
+  async updateFood() {
+    const response = await apiClient.patch(`/foods/4`, {
+      name: "ffff",
+      price: 5,
+      categoryId: 2,
+      imageUrl: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd",
+      status: "AVAILABLE"
+    });
+    return response.data;
+  },
 };
